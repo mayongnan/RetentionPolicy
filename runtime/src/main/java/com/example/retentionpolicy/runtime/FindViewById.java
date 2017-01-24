85067dfb05a22d1c2c997d6f6ee77b9581c7cd5d
mayongnan <myskyn@163.com>
2017-01-24 17:07:05 +0800

    添加注释

--- runtime/src/main/java/com/example/retentionpolicy/runtime/FindViewById.java
+++ runtime/src/main/java/com/example/retentionpolicy/runtime/FindViewById.java
@@ -9,8 +9,12 @@ import java.lang.annotation.Target;
  * 自定义View绑定注解
  * Created by YON on 2017/1/18.
  */
+
+//声明注解范围为成员变量
 @Target(ElementType.FIELD)
+//声明注解为运行时注解
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FindViewById {
+    //定义一个int类型的元素
     int value();
 }
